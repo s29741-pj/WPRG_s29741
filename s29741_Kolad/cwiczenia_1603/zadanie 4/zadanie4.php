4f1d6a791ea106e6ec318592365ae163847a805d
s29741-pj <s29741@pjwstk.edu.pl>
2025-03-28 09:14:26 +0100

    task 4 done

--- s29741_Kolad/cwiczenia_1603/zadanie 4/zadanie4.php
+++ s29741_Kolad/cwiczenia_1603/zadanie 4/zadanie4.php
@@ -1,6 +1,54 @@
 <?php
 
 $formVisible = "";
+$primaryVisible = "hidden";
+$isPrimary = false;
+$loop1 = 0;
+$loop2 = 0;
+$loop3 = 0;
+
+if (isset($_POST['primary'])) {
+    $number = $_POST['number'];
+
+    function checkIfPrimal($number) {
+        global $isPrimary, $loop1, $loop2, $loop3, $primaryVisible;
+
+        if ($number <= 1) {
+            $isPrimary = false;
+            return;
+        }
+
+        for ($i = 2; $i <= $number; $i++) {
+            $loop1+=1;
+            if ($number % $i === 0) {
+                $isPrimary = false;
+                break;
+            }
+            $isPrimary = true;
+        }
+
+        for ($i = 2; $i <= $number/2; $i++) {
+            $loop2+=1;
+            if ($number % $i === 0) {
+                $isPrimary = false;
+                break;
+            }
+            $isPrimary = true;
+        }
+
+        for ($i = 2; $i <= sqrt($number); $i++) {
+            $loop3 += 1;
+            if ($number % $i === 0) {
+                $isPrimary = false;
+                break;
+            }
+            $isPrimary = true;
+        }
+
+        $primaryVisible = "";
+    }
+    checkIfPrimal($number);
+}
 
 
 ?>
@@ -24,73 +72,39 @@ $formVisible = "";
     <title>zadanie 1</title>
     <link rel="stylesheet" href="./style.css">
     <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-        rel="stylesheet">
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+            rel="stylesheet">
 
 </head>
 <body>
+<!--tu stop 27.03 dodać logikę-->
 
 <div class="divBox <?php echo $formVisible; ?>">
     <fieldset class="outer-fieldset">
         <legend>Czy pierwsza?</legend>
-        <form name="primary" action="zadanie2.php" method="POST">
+        <form name="primary" action="zadanie4.php" method="POST">
             <input type="hidden" name="primary" value="Functions">
             <div class="innerDiv">
-                <h3>Dane podstawowe</h3>
-                <label for="guests">Podaj liczbę gości:</label>
-                <select name="guests" id="guests">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                </select>
-                <label for="name">*Imię:</label>
-                <input type="text" name="name" id="name" required>
-                <label for="surname">*Nazwisko:</label>
-                <input type="text" name="surname" id="surname" required>
-                <label for="address">*Adres:</label>
-                <input type="text" name="address" id="address" required>
-                <label for="card">*Numer karty kredytowej:</label>
-                <input type="number" name="card" id="card" required>
-                <label for="email">*Adres email:</label>
-                <input type="email" name="email" id="email" required>
+                <label for="card">Sprawdzana liczba:</label>
+                <input type="number" name="number" id="card" required>
             </div>
             <div class="innerDiv">
-                <h3>Czas pobytu</h3>
-                <label for="arrivalDate">*Data przyjazdu</label>
-                <input type="date" name="arrivalDate" id="arrivalDate" required>
-                <label for="arrivalHour">*Godzina przyjazdu</label>
-                <input type="time" name="arrivalHour" id="arrivalHour" required>
-                <label for="departureDate">*Data wyjazdu</label>
-                <input type="date" name="departureDate" id="departureDate" required>
+                <div class="summary-details"><p>czy pierwsza?</p>
+                    <p class="<?php echo $primaryVisible ?>"
+                       style="color: <?= $isPrimary ? 'green' : 'red' ?>; text-transform: uppercase;">
+                        <?= $isPrimary ? 'tak' : 'nie' ?>
+                    </p></div>
+                <h4>iteracje</h4>
+                <div class="summary-details"><p>od 2 do n</p>
+                    <p><?php echo $loop1 ?></p></div>
+                <div class="summary-details"><p>do n/2</p>
+                    <p><?php echo $loop2 ?></p></div>
+                <div class="summary-details"><p>od 2 do &Sqrt;n</p>
+                    <p><?php echo $loop3 ?></p></div>
             </div>
-
-            <fieldset class="inner-fieldset">
-                <legend>Opcje dodatkowe i udogodnienia</legend>
-                <ul>
-                    <li>
-                        <input type="checkbox" name="bed" id="bed">
-                        <label for="bed">Czy potrzebna będzie dostawka?</label>
-                    </li>
-                    <li>
-                        <input type="checkbox" name="ac" id="ac">
-                        <label for="ac">Klimatyzacja</label>
-                    </li>
-                    <li>
-                        <input type="checkbox" name="champagne" id="champagne">
-                        <label for="champagne">Szampan</label>
-                    </li>
-                    <li>
-                        <input type="checkbox" name="strawberries" id="strawberries">
-                        <label for="strawberries">Truskawki</label>
-                    </li>
-                </ul>
-            </fieldset>
             <input type="submit" name="submit" id="submit" class="submit">
         </form>
-
     </fieldset>
 </div>
-
 </body>
 </html>
